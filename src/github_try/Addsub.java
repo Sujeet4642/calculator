7cf3ca8177fd1706409d55a6cf17833513da5393
Sujeet4642 <sujeet22s1990@gmail.com>
2020-08-04 23:07:04 +0530

    addition and substration

---
+++ src/github_try/Addsub.java
@@ -0,0 +1,26 @@
+package github_try;
+
+import java.util.Scanner;
+
+public class Addsub {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int a;
+		int b;
+		int result;
+		System.out.println("Enter first and second digits");
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		if (a>b) {
+			result = a + b;
+			System.out.println("Sum of digits : " + result);
+			}
+		else {
+			result = a - b;
+			System.out.println("sub of digits : " + result);
+			}
+	
+	}
+
+}
