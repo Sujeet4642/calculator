3f4c51800ee282a67a0ac571e1617f5f19cd8600
Sujeet4642 <sujeet22s1990@gmail.com>
2020-08-05 01:16:54 +0530

    adding operator multiply and division

--- src/github_try/Operations.java
+++ src/github_try/Operations.java
@@ -3,23 +3,46 @@ package github_try;
 import java.util.Scanner;
 
 public class Operations {
+
+
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter first numer");
 		int a;
-		int b;
-		int result;
-		System.out.println("Enter first and second digits");
 		a = sc.nextInt();
+		
+		System.out.println("Enter second numer");
+		int b;
 		b = sc.nextInt();
 		
-		if (a>b) {
-			result = a + b;
+		System.out.println("Enter operator (+,-,*,/): ");
+		char operator;
+		operator = sc.next().charAt(0);
+		
+		
+		
+		
+		if (operator== '+') {
+			int result = a + b;
 			System.out.println("Sum of digits : " + result);
 			}
-		else {
-			result = a - b;
+		else if(operator== '-') {
+			int result = a - b;
 			System.out.println("sub of digits : " + result);
 			}
+		else if(operator== '*') {
+			int result = a * b;
+			System.out.println("multiply of digits : " + result);
+			}
+		else if(operator== '/') {
+			int result = a / b;
+			System.out.println("division of digits : " + result);
+			}
+		
+		else {
+			System.out.println("invalid operator");
+		}
 	
 	}
 
